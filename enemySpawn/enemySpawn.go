6e128ca16baa80a981c0c017634298e66011ab66
Tobias Gärdebro <tubbeg@gmail.com>
2025-11-10 23:17:52 +0100

    Added factory object for enemies. Added collisions as well. Still thinking about the best way to handle collisions since there will be many objects on screen.

---
+++ enemySpawn/enemySpawn.go
@@ -0,0 +1,10 @@
+components {
+  id: "spawn"
+  component: "/enemySpawn/spawn.script"
+}
+embedded_components {
+  id: "factory"
+  type: "factory"
+  data: "prototype: \"/enemy/enemy.go\"\n"
+  ""
+}
