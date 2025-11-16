0c16a9419d18fefbc04d8acfc9fa83259a126a66
Tobias Gärdebro <tubbeg@gmail.com>
2025-11-16 19:06:42 +0100

    Added collision groups, but collisions still use the messaging system. Added camera game object.

---
+++ cam/cam.go
@@ -0,0 +1,14 @@
+components {
+  id: "cam"
+  component: "/cam/cam.script"
+}
+embedded_components {
+  id: "camera"
+  type: "camera"
+  data: "aspect_ratio: 1.0\n"
+  "fov: 0.7854\n"
+  "near_z: 0.1\n"
+  "far_z: 1000.0\n"
+  "orthographic_projection: 1\n"
+  ""
+}
